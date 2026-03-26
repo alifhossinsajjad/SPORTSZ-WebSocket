d636eae233068ab24866b97a8a955cac0ec0053e
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2026-03-26 12:46:06 +0700

    solve the server issues

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import express from "express";
+import cors from "cors";
+import { matchRouter } from "./routes/matches.js";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Sports Server run successfully");
 });
+
+app.use('/matches', matchRouter)
+
+
 
 app.listen(port, () => {
   console.log(`server running on the : http://localhost:${port}`);
