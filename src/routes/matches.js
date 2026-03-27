652e279d49f688f092ba5abea7edd63b06f4fe59
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2026-03-28 00:47:20 +0700

    post mehtod work successfully and data are save into the neon database successfully

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { createMatchSchema } from "../validation/matches";
+import { createMatchSchema } from "../validation/matches.js";
+import { db } from "../db/db.js";
+import { matches } from "../db/schema.js";
+import { getMatchStatus } from "../utils/match-status.js";
 
 export const matchRouter = Router();
 
@@ -7,6 +10,37 @@ matchRouter.get("/", (req, res) => {
   res.status(200).json({ message: "Match list" });
 });
 
-matchRouter.post("/", (req, res) => {
+matchRouter.post("/", async (req, res) => {
   const parsed = createMatchSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    return res.status(400).json({
+      error: "invalid payload.",
+      details: parsed.error.issues,
+    });
+  }
+
+  const {
+    data: { startTime, endTime, homeScore, awayScore },
+  } = parsed;
+
+  try {
+    const [event] = await db
+      .insert(matches)
+      .values({
+        ...parsed.data,
+        startTime: new Date(parsed.data.startTime),
+        endTime: new Date(parsed.data.endTime),
+        homeScore: homeScore ?? 0,
+        awayScore: awayScore ?? 0,
+        status: getMatchStatus(startTime, endTime),
+      })
+      .returning();
+    res.status(201).json({ data: event });
+  } catch (e) {
+    res.status(500).json({
+      error: "Failed to create match .",
+      details: e.message,
+    });
+  }
 });
